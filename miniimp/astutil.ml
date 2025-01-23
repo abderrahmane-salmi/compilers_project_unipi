2684df173c5cf424caf36171bf8e882686888bf9
Abderrahmane Salmi <abderrahmane.salmi.off@gmail.com>
2025-01-23 14:09:08 +0100

    separated the print funs to separate util file

---
+++ miniimp/astutil.ml
@@ -0,0 +1,23 @@
+let rec string_of_aexp (a: Ast.aexp) : string =
+  match a with
+  | Ast.Num n -> string_of_int n
+  | Ast.Var v -> v
+  | Ast.Plus (a1, a2) -> "(" ^ string_of_aexp a1 ^ " + " ^ string_of_aexp a2 ^ ")"
+  | Ast.Minus (a1, a2) -> "(" ^ string_of_aexp a1 ^ " - " ^ string_of_aexp a2 ^ ")"
+  | Ast.Times (a1, a2) -> "(" ^ string_of_aexp a1 ^ " * " ^ string_of_aexp a2 ^ ")"
+  
+let rec string_of_bexp (b: Ast.bexp) : string =
+  match b with
+  | Ast.Bool b -> string_of_bool b
+  | Ast.And (b1, b2) -> "(" ^ string_of_bexp b1 ^ " and " ^ string_of_bexp b2 ^ ")"
+  | Ast.Not b -> "not " ^ string_of_bexp b
+  | Ast.Less (a1, a2) -> "(" ^ string_of_aexp a1 ^ " < " ^ string_of_aexp a2 ^ ")"
+  
+let rec string_of_com (com: Ast.com) : string =
+  match com with
+  | Ast.Skip -> "skip"
+  | Ast.Assign (v, a) -> v ^ " := " ^ string_of_aexp a
+  | Ast.BQuestion b -> string_of_bexp b ^ "?"
+  | Ast.Seq (c1, c2) -> string_of_com c1 ^ " ; " ^ string_of_com c2
+  | Ast.If (b, c1, c2) -> "if " ^ string_of_bexp b ^ " then " ^ string_of_com c1 ^ " else " ^ string_of_com c2
+  | Ast.While (b, c) -> "while " ^ string_of_bexp b ^ " do " ^ string_of_com c
--- miniimp/main.ml
+++ miniimp/main.ml
@@ -1,30 +1,5 @@
 open Semantics
-open Ast
 open Cfg
-
-let rec string_of_aexp (a: Ast.aexp) : string =
-match a with
-| Ast.Num n -> string_of_int n
-| Ast.Var v -> v
-| Ast.Plus (a1, a2) -> "(" ^ string_of_aexp a1 ^ " + " ^ string_of_aexp a2 ^ ")"
-| Ast.Minus (a1, a2) -> "(" ^ string_of_aexp a1 ^ " - " ^ string_of_aexp a2 ^ ")"
-| Ast.Times (a1, a2) -> "(" ^ string_of_aexp a1 ^ " * " ^ string_of_aexp a2 ^ ")"
-
-let rec string_of_bexp (b: Ast.bexp) : string =
-match b with
-| Ast.Bool b -> string_of_bool b
-| Ast.And (b1, b2) -> "(" ^ string_of_bexp b1 ^ " and " ^ string_of_bexp b2 ^ ")"
-| Ast.Not b -> "not " ^ string_of_bexp b
-| Ast.Less (a1, a2) -> "(" ^ string_of_aexp a1 ^ " < " ^ string_of_aexp a2 ^ ")"
-
-let rec string_of_com (com: Ast.com) : string =
-  match com with
-  | Ast.Skip -> "skip"
-  | Ast.Assign (v, a) -> v ^ " := " ^ string_of_aexp a
-  | Ast.BQuestion b -> string_of_bexp b ^ "?"
-  | Ast.Seq (c1, c2) -> string_of_com c1 ^ " ; " ^ string_of_com c2
-  | Ast.If (b, c1, c2) -> "if " ^ string_of_bexp b ^ " then " ^ string_of_com c1 ^ " else " ^ string_of_com c2
-  | Ast.While (b, c) -> "while " ^ string_of_bexp b ^ " do " ^ string_of_com c
 
 (* Main function to read program, generate CFG, and evaluate it *)
 let () =
@@ -69,7 +44,7 @@ let () =
   List.iter (fun node ->
     match node with
     | BasicBlock (id, code) -> 
-        Printf.printf "Node %d: %s\n" id (string_of_com code)
+        Printf.printf "Node %d: %s\n" id (Astutil.string_of_com code)
   ) cfg.nodes;
 
   (* Print the edges *)
