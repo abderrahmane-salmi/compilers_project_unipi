2dc69d54c511c67a76e0b5210b959a278ec1cfab
Abderrahmane Salmi <abderrahmane.salmi.off@gmail.com>
2025-01-23 15:01:05 +0100

    removed paranthesis in string to com util funs

--- miniimp/astutil.ml
+++ miniimp/astutil.ml
@@ -6,16 +6,16 @@ let rec string_of_aexp (a: Ast.aexp) : string =
   match a with
   | Ast.Num n -> string_of_int n
   | Ast.Var v -> v
-  | Ast.Plus (a1, a2) -> "(" ^ string_of_aexp a1 ^ " + " ^ string_of_aexp a2 ^ ")"
-  | Ast.Minus (a1, a2) -> "(" ^ string_of_aexp a1 ^ " - " ^ string_of_aexp a2 ^ ")"
-  | Ast.Times (a1, a2) -> "(" ^ string_of_aexp a1 ^ " * " ^ string_of_aexp a2 ^ ")"
+  | Ast.Plus (a1, a2) -> string_of_aexp a1 ^ " + " ^ string_of_aexp a2
+  | Ast.Minus (a1, a2) -> string_of_aexp a1 ^ " - " ^ string_of_aexp a2
+  | Ast.Times (a1, a2) -> string_of_aexp a1 ^ " * " ^ string_of_aexp a2
   
 let rec string_of_bexp (b: Ast.bexp) : string =
   match b with
   | Ast.Bool b -> string_of_bool b
-  | Ast.And (b1, b2) -> "(" ^ string_of_bexp b1 ^ " and " ^ string_of_bexp b2 ^ ")"
+  | Ast.And (b1, b2) -> string_of_bexp b1 ^ " and " ^ string_of_bexp b2
   | Ast.Not b -> "not " ^ string_of_bexp b
-  | Ast.Less (a1, a2) -> "(" ^ string_of_aexp a1 ^ " < " ^ string_of_aexp a2 ^ ")"
+  | Ast.Less (a1, a2) -> string_of_aexp a1 ^ " < " ^ string_of_aexp a2
   
 let rec string_of_com (com: Ast.com) : string =
   match com with
